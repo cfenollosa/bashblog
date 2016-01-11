18d2df26ce54c96cb0c95a16b8cf7b5da65e2c5e
Martijn Dekker <martijn@inlv.org>
2016-01-11 18:56:39 +0100

    fix: allow field splitting on tags_with_posts call
    Quoted one too many there.

--- bb.sh
+++ bb.sh
@@ -323,7 +323,7 @@ edit() {
     tags_after=$(tags_in_post "$filename")
     relevant_tags=$(echo "$tags_before $tags_after" | tr ',' ' ' | tr ' ' '\n' | sort -u | tr '\n' ' ')
     if [[ ! -z $relevant_tags ]]; then
-        relevant_posts="$(posts_with_tags "$relevant_tags") $filename"
+        relevant_posts="$(posts_with_tags $relevant_tags) $filename"
         rebuild_tags "$relevant_posts" "$relevant_tags"
     fi
 }
