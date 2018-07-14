b1c538a1a76a3325c350edfcecd1c36cbb3a26ee
Dknight <smirnov.dmitri@gmail.com>
2018-07-14 05:14:07 +0300

    Add missing quote

--- bb.sh
+++ bb.sh
@@ -938,7 +938,7 @@ create_includes() {
     else {
         echo '<!DOCTYPE html>'
         echo '<html><head>'
-        echo '<meta charset=UTF-8">'
+        echo '<meta charset="UTF-8">'
         echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
         printf '<link rel="stylesheet" href="%s" type="text/css">\n' "${css_include[@]}"
         if [[ -z $global_feedburner ]]; then
